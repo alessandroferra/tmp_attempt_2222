c7bcf42fb088403e3282f3477cb2b2e67f850f66
alessandroferra <alessandro.ferrari.1705@gmail.com>
2022-05-20 13:33:37 +0200

    first

---
+++ custom_components/myhome_timed/cover.py
@@ -0,0 +1,394 @@
+"""Support for MyHome covers."""
+import voluptuous as vol
+
+from datetime import timedelta
+
+from homeassistant.core import callback
+from homeassistant.helpers.event import async_track_utc_time_change, async_track_time_interval
+from homeassistant.components.cover import (
+    ATTR_CURRENT_POSITION,
+    ATTR_POSITION,
+    PLATFORM_SCHEMA,
+    DOMAIN as PLATFORM,
+    SUPPORT_CLOSE,
+    SUPPORT_OPEN,
+    SUPPORT_SET_POSITION,
+    SUPPORT_STOP,
+    CoverDeviceClass,
+    CoverEntity,
+)
+
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_DEVICES,
+    CONF_ENTITIES,
+)
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.restore_state import RestoreEntity
+
+from OWNd.message import (
+    OWNAutomationEvent,
+    OWNAutomationCommand,
+)
+
+from .const import (
+    CONF,
+    CONF_GATEWAY,
+    CONF_WHO,
+    CONF_WHERE,
+    CONF_MANUFACTURER,
+    CONF_DEVICE_MODEL,
+    CONF_ADVANCED_SHUTTER,
+    DOMAIN,
+    LOGGER,
+)
+from .myhome_device import MyHOMEEntity
+from .gateway import MyHOMEGatewayHandler
+
+CONF_TIMED_SHUTTER = 'timed'
+CONF_TRAVELLING_TIME_DOWN = 'travelling_time_down'
+CONF_TRAVELLING_TIME_UP = 'travelling_time_up'
+DEFAULT_TRAVEL_TIME = 25
+
+MYHOME_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_WHERE): cv.string,
+        vol.Optional(CONF_NAME): cv.string,
+        vol.Optional(CONF_TIMED_SHUTTER): cv.boolean,
+        vol.Optional(CONF_TRAVELLING_TIME_DOWN, default=DEFAULT_TRAVEL_TIME): cv.positive_int,
+        vol.Optional(CONF_TRAVELLING_TIME_UP, default=DEFAULT_TRAVEL_TIME): cv.positive_int,
+        vol.Optional(CONF_ADVANCED_SHUTTER): cv.boolean,
+        vol.Optional(CONF_MANUFACTURER): cv.string,
+        vol.Optional(CONF_DEVICE_MODEL): cv.string,
+    }
+)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {vol.Required(CONF_DEVICES, default={}): cv.schema_with_slug_keys(MYHOME_SCHEMA)}
+)
+
+
+async def async_setup_platform(
+    hass, config, async_add_entities, discovery_info=None
+):  # pylint: disable=unused-argument
+    if CONF not in hass.data[DOMAIN]:
+        return False
+    hass.data[DOMAIN][CONF][PLATFORM] = {}
+    _configured_covers = config.get(CONF_DEVICES)
+
+    if _configured_covers:
+        for _, entity_info in _configured_covers.items():
+            who = "2"
+            where = entity_info[CONF_WHERE]
+            device_id = f"{who}-{where}"
+            name = (
+                entity_info[CONF_NAME]
+                if CONF_NAME in entity_info
+                else f"A{where[:len(where)//2]}PL{where[len(where)//2:]}"
+            )
+            timed = (
+                entity_info[CONF_TIMED_SHUTTER]
+                if CONF_TIMED_SHUTTER in entity_info
+                else False
+            )
+            travel_time_down = entity_info[CONF_TRAVELLING_TIME_DOWN]
+            travel_time_up= entity_info[CONF_TRAVELLING_TIME_UP]
+            where = entity_info[CONF_WHERE]
+            advanced = (
+                entity_info[CONF_ADVANCED_SHUTTER]
+                if CONF_ADVANCED_SHUTTER in entity_info
+                else False
+            )
+            entities = []
+            manufacturer = (
+                entity_info[CONF_MANUFACTURER]
+                if CONF_MANUFACTURER in entity_info
+                else None
+            )
+            model = (
+                entity_info[CONF_DEVICE_MODEL]
+                if CONF_DEVICE_MODEL in entity_info
+                else None
+            )
+            hass.data[DOMAIN][CONF][PLATFORM][device_id] = {
+                CONF_WHO: who,
+                CONF_WHERE: where,
+                CONF_ENTITIES: entities,
+                CONF_NAME: name,
+                CONF_TIMED_SHUTTER: timed,
+                CONF_TRAVELLING_TIME_DOWN: travel_time_down,
+                CONF_TRAVELLING_TIME_UP: travel_time_up,
+                CONF_ADVANCED_SHUTTER: advanced,
+                CONF_MANUFACTURER: manufacturer,
+                CONF_DEVICE_MODEL: model,
+            }
+
+
+async def async_setup_entry(
+    hass, config_entry, async_add_entities
+):  # pylint: disable=unused-argument
+    if PLATFORM not in hass.data[DOMAIN][CONF]:
+        return True
+
+    _covers = []
+    _configured_covers = hass.data[DOMAIN][CONF][PLATFORM]
+
+    for _cover in _configured_covers.keys():
+        _cover = MyHOMECover(
+            hass=hass,
+            device_id=_cover,
+            who=_configured_covers[_cover][CONF_WHO],
+            where=_configured_covers[_cover][CONF_WHERE],
+            name=_configured_covers[_cover][CONF_NAME],
+            timed=_configured_covers[_cover][CONF_TIMED_SHUTTER],
+            travel_time_down=_configured_covers[_cover][CONF_TRAVELLING_TIME_DOWN],
+            travel_time_up=_configured_covers[_cover][CONF_TRAVELLING_TIME_UP],
+            advanced=_configured_covers[_cover][CONF_ADVANCED_SHUTTER],
+            manufacturer=_configured_covers[_cover][CONF_MANUFACTURER],
+            model=_configured_covers[_cover][CONF_DEVICE_MODEL],
+            gateway=hass.data[DOMAIN][CONF_GATEWAY],
+        )
+        _covers.append(_cover)
+
+    async_add_entities(_covers)
+
+
+async def async_unload_entry(hass, config_entry):  # pylint: disable=unused-argument
+    if PLATFORM not in hass.data[DOMAIN][CONF]:
+        return True
+
+    _configured_covers = hass.data[DOMAIN][CONF][PLATFORM]
+
+    for _cover in _configured_covers.keys():
+        del hass.data[DOMAIN][CONF_ENTITIES][_cover]
+
+
+class MyHOMECover(MyHOMEEntity, CoverEntity):
+
+    device_class = CoverDeviceClass.SHUTTER
+
+    def __init__(
+        self,
+        hass,
+        name: str,
+        device_id: str,
+        who: str,
+        where: str,
+        timed: bool,
+        travel_time_down: int,
+        travel_time_up: int,
+        advanced: bool,
+        manufacturer: str,
+        model: str,
+        gateway: MyHOMEGatewayHandler,
+    ):
+        super().__init__(
+            hass=hass,
+            name=name,
+            device_id=device_id,
+            who=who,
+            where=where,
+            manufacturer=manufacturer,
+            model=model,
+            gateway=gateway,
+        )
+
+        from xknx.devices import TravelCalculator
+        self._travel_time_down = travel_time_down
+        self._travel_time_up = travel_time_up
+
+        self._attr_supported_features = SUPPORT_OPEN | SUPPORT_CLOSE | SUPPORT_STOP
+        if advanced:
+            self._attr_supported_features |= SUPPORT_SET_POSITION
+        if timed:
+            self._attr_supported_features |= SUPPORT_SET_POSITION
+        self._gateway_handler = gateway
+
+        self._unsubscribe_auto_updater = None
+
+        self.tc = TravelCalculator(self._travel_time_down, self._travel_time_up)
+
+        self._attr_extra_state_attributes = {
+            "A": where[: len(where) // 2],
+            "PL": where[len(where) // 2 :],
+        }
+
+        self._attr_current_cover_position = None
+        self._attr_is_opening = None
+        self._attr_is_closing = None
+        self._attr_is_closed = None
+
+    async def async_added_to_hass(self):
+        """ Only cover's position matters.             """
+        """ The rest is calculated from this attribute."""
+        old_state = await self.async_get_last_state()
+        _LOGGER.debug('async_added_to_hass :: oldState %s', old_state)
+        if (
+                old_state is not None and
+                self.tc is not None and
+                old_state.attributes.get(ATTR_CURRENT_POSITION) is not None):
+            self.tc.set_position(int(
+                old_state.attributes.get(ATTR_CURRENT_POSITION)))
+
+    @property
+    def device_state_attributes(self):
+        """Return the device state attributes."""
+        attr = {}
+        if self._travel_time_down is not None:
+            attr[CONF_TRAVELLING_TIME_DOWN] = self._travel_time_down
+        if self._travel_time_up is not None:
+            attr[CONF_TRAVELLING_TIME_UP] = self._travel_time_up
+        return attr
+
+    @property
+    def current_cover_position(self):
+        """Return the current position of the cover."""
+        return self.tc.current_position()
+
+    @property
+    def is_opening(self):
+        """Return if the cover is opening or not."""
+        from xknx.devices import TravelStatus
+        return self.tc.is_traveling() and \
+               self.tc.travel_direction == TravelStatus.DIRECTION_UP
+
+    @property
+    def is_closing(self):
+        """Return if the cover is closing or not."""
+        from xknx.devices import TravelStatus
+        return self.tc.is_traveling() and \
+               self.tc.travel_direction == TravelStatus.DIRECTION_DOWN
+
+    @property
+    def is_closed(self):
+        """Return if the cover is closed."""
+        return self.tc.is_closed()
+
+    @property
+    def assumed_state(self):
+        """Return True because covers can be stopped midway."""
+        return True
+
+    async def async_update(self):
+        """Update the entity.
+
+        Only used by the generic entity update service.
+        """
+        await self._gateway_handler.send_status_request(
+            OWNAutomationCommand.status(self._where)
+        )
+
+    async def async_open_cover(self, **kwargs):  # pylint: disable=unused-argument
+        """Open the cover."""
+        _LOGGER.debug('async_open_cover')
+        self.tc.start_travel_up()
+
+        self.start_auto_updater()
+        await self._gateway_handler.send(
+            OWNAutomationCommand.raise_shutter(self._where)
+        )
+
+    async def async_close_cover(self, **kwargs):  # pylint: disable=unused-argument
+        """Close cover."""
+        _LOGGER.debug('async_close_cover')
+        self.tc.start_travel_down()
+
+        self.start_auto_updater()
+        await self._gateway_handler.send(
+            OWNAutomationCommand.lower_shutter(self._where)
+        )
+
+    async def async_set_cover_position(self, **kwargs):
+        """Move the cover to a specific position."""
+        if ATTR_POSITION in kwargs and CONF_TIMED_SHUTTER not in kwargs:
+            position = kwargs[ATTR_POSITION]
+            await self._gateway_handler.send(
+                OWNAutomationCommand.set_shutter_level(self._where, position)
+            )
+        elif ATTR_POSITION in kwargs and CONF_TIMED_SHUTTER in kwargs:
+            position = kwargs[ATTR_POSITION]
+            _LOGGER.debug('async_set_cover_position: %d', position)
+            await self.set_position(position)
+
+
+    async def async_stop_cover(self, **kwargs):  # pylint: disable=unused-argument
+        """Stop the cover."""
+        _LOGGER.debug('async_stop_cover')
+        if self.tc.is_traveling():
+            _LOGGER.debug('_handle_my_button :: button stops cover')
+            self.tc.stop()
+            self.stop_auto_updater()
+        await self._gateway_handler.send(OWNAutomationCommand.stop_shutter(self._where))
+
+    async def set_position(self, position):
+        _LOGGER.debug('set_position')
+        """Move cover to a designated position."""
+        current_position = self.tc.current_position()
+        _LOGGER.debug('set_position :: current_position: %d, new_position: %d',
+                      current_position, position)
+        command = None
+        if position < current_position:
+            self.start_auto_updater()
+            self.tc.start_travel(position)
+            _LOGGER.debug('set_position :: command %s', command)
+            await self._gateway_handler.send(
+                OWNAutomationCommand.raise_shutter(self._where)
+            )
+        elif position > current_position:
+            self.start_auto_updater()
+            self.tc.start_travel(position)
+            _LOGGER.debug('set_position :: command %s', command)
+            await self._gateway_handler.send(
+                OWNAutomationCommand.lower_shutter(self._where)
+            )
+        return
+
+    def start_auto_updater(self):
+        """Start the autoupdater to update HASS while cover is moving."""
+        _LOGGER.debug('start_auto_updater')
+        if self._unsubscribe_auto_updater is None:
+            _LOGGER.debug('init _unsubscribe_auto_updater')
+            interval = timedelta(seconds=0.1)
+            self._unsubscribe_auto_updater = async_track_time_interval(
+                self.hass, self.auto_updater_hook, interval)
+
+    @callback
+    def auto_updater_hook(self, now):
+        """Call for the autoupdater."""
+        _LOGGER.debug('auto_updater_hook')
+        self.async_schedule_update_ha_state()
+        if self.position_reached():
+            _LOGGER.debug('auto_updater_hook :: position_reached')
+            self.stop_auto_updater()
+        self.hass.async_create_task(self.auto_stop_if_necessary())
+
+    def stop_auto_updater(self):
+        """Stop the autoupdater."""
+        _LOGGER.debug('stop_auto_updater')
+        if self._unsubscribe_auto_updater is not None:
+            self._unsubscribe_auto_updater()
+            self._unsubscribe_auto_updater = None
+
+    def position_reached(self):
+        """Return if cover has reached its final position."""
+        return self.tc.position_reached()
+
+    async def auto_stop_if_necessary(self):
+        """Do auto stop if necessary."""
+        if self.position_reached():
+            _LOGGER.debug('auto_stop_if_necessary :: calling stop command')
+            await self._gateway_handler.send(OWNAutomationCommand.stop_shutter(self._where))
+            self.tc.stop()
+
+    def handle_event(self, message: OWNAutomationEvent):
+        """Handle an event message."""
+        LOGGER.info(message.human_readable_log)
+        self._attr_is_opening = message.is_opening
+        self._attr_is_closing = message.is_closing
+        if message.is_closed is not None:
+            self._attr_is_closed = message.is_closed
+        if message.current_position is not None:
+            self._attr_current_cover_position = message.current_position
+
+        self.async_schedule_update_ha_state()
